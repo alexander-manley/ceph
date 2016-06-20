e359de5d4cd5ad2b9452b064751163ff50b3749d
Yan, Zheng <zyan@redhat.com>
2016-06-20 15:20:44 +0800

    librbd: include proper header for __le{16,32,64}
    __le{16,32,64} are used in include/rbd_types.h
    
    Signed-off-by: Yan, Zheng <zyan@redhat.com>

--- src/librbd/Utils.cc
+++ src/librbd/Utils.cc
@@ -1,8 +1,8 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
 // vim: ts=8 sw=2 smarttab
 
+#include "include/types.h"
 #include "librbd/Utils.h"
-#include "include/rbd_types.h"
 #include "include/stringify.h"
 
 namespace librbd {
